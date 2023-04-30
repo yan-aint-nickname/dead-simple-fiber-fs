79ec4079c4331db7d51c9c0f2395f38240d6bfcc
Yan Kirsh <yankirsh@yandex.ru>
2023-04-30 23:30:17 +0300

    Bugfix with prefork and alpine image, add compose file

--- Dockerfile
+++ Dockerfile
@@ -2,10 +2,10 @@ FROM golang:1.20.3-alpine3.17
 
 WORKDIR /app
 
-COPY . .
+COPY main.go main.go
+COPY go.mod go.mod
+COPY go.sum go.sum
 
 RUN go build
-
-EXPOSE 3000
 
 CMD ["sh", "-c", "./dead-simple-fiber-fs"]
--- main.go
+++ main.go
@@ -9,16 +9,14 @@ import (
 )
 
 func main() {
-	app := fiber.New(fiber.Config{
-		Prefork: true,
-	})
+	app := fiber.New()
 
 	app.Post("file/", func(c *fiber.Ctx) error {
 		file, err := c.FormFile("image")
 		if err != nil {
 			return err
 		}
-		path := fmt.Sprintf("files/%s", file.Filename)
+		path := fmt.Sprintf("/files/%s", file.Filename)
 		if err := c.SaveFile(file, path); err != nil {
 			return err
 		}
@@ -27,7 +25,7 @@ func main() {
 
 	app.Delete("file/:path", func(c *fiber.Ctx) error {
 		path := c.Params("path")
-		fullPath := fmt.Sprintf("files/%s", path)
+		fullPath := fmt.Sprintf("/files/%s", path)
 		if err := os.Remove(fullPath); err != nil {
 			return err
 		}
